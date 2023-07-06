74644f90a776631b6508f8364cff47695169a20e
Metul <84008206+MeTuL10@users.noreply.github.com>
2023-07-06 12:35:52 +0530

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -64,6 +64,10 @@ def create_accounts():
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
+    """
+    Creates an Account
+    This endpoint will create an Account based the data in the body that is posted
+    """
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
@@ -76,6 +80,10 @@ def list_accounts():
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
+    """
+    Creates an Account
+    This endpoint will create an Account based the data in the body that is posted
+    """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -90,6 +98,10 @@ def get_accounts(account_id):
 # ... place you code here to UPDATE an account ...
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
+    """
+    Creates an Account
+    This endpoint will create an Account based the data in the body that is posted
+    """
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
@@ -105,7 +117,10 @@ def update_accounts(account_id):
 # ... place you code here to DELETE an account ...
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
-
+    """
+    Creates an Account
+    This endpoint will create an Account based the data in the body that is posted
+    """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
