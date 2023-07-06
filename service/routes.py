d1c8dea5743ccb6794ee6337fd7e10cddb5e0c04
Metul <84008206+MeTuL10@users.noreply.github.com>
2023-07-06 12:38:40 +0530

    completes ci build

--- service/routes.py
+++ service/routes.py
@@ -65,12 +65,14 @@ def create_accounts():
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    List all accounts
+    This endpoints will list all Accounts
     """
-    app.logger.info("Request to list Accounts")
+    app.logger.info("Request to lista accounts")
+
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -78,18 +80,17 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+# ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def get_accounts(account_id):
+def read_accounts(account_id):
     """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    Read a account
     """
-    app.logger.info("Request to read an Account with id: %s", account_id)
+    app.logger.info("Request to update account id %s", account_id)
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -99,10 +100,10 @@ def get_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    Update an Accounts
     """
-    app.logger.info("Request to update an Account with id: %s", account_id)
+    app.logger.info("Request to update account id %s", account_id)
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
@@ -118,8 +119,8 @@ def update_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
